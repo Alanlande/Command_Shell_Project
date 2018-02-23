51b4022ba41fd011399fd14c679e9e1b0a090164
De Lan <dl261@duke.edu>
2018-02-23 18:03:49 -0500

    final

---
+++ testcases/step4/printf.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main(int argc, char ** argv){
+  //std::string inputFile;
+  for (int i = 0 ; i < 3000;i++)
+    std::cout<<"\nYes, printfDir program in directory /testcase/step4 works!\n";
+  /*
+  for (int i = 1; i < argc; i++){
+    std::cout << "(" << argv[i] << ") ";
+  }
+  std::cout<<"\nThis is the end of args.\n";
+  
+  while (std::getline (std::cin, inputFile)){
+    std::cout <<inputFile<<std::endl;
+  }
+  std::cout<<"This is the end of std::cin.\n";
+  if (!std::cin){
+    std::cerr << "std::cin fails!" << std::endl;
+  }
+  */
+  return EXIT_SUCCESS;
+}
